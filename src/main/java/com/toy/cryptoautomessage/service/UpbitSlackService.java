b6b29ce80474ec263e37a16a8e044b9e26568c21
2-seulgi <90025449+2-seulgi@users.noreply.github.com>
2024-08-05 01:56:54 +0900

    ✨ 스프링 배치 활용해서 메세지 전송 및 데이터 저장

--- src/main/java/com/toy/cryptoautomessage/service/UpbitSlackService.java
+++ src/main/java/com/toy/cryptoautomessage/service/UpbitSlackService.java
@@ -25,12 +25,14 @@ public class UpbitSlackService {
 
 
         // 메세지 발송
-//        StringBuilder sb = new StringBuilder();
-//        sb.append("실시간 데이터 ");
-//        sb.append(market);
-//        sb.append(" 비트코인 가격 : " );
-//        sb.append(formatPrice);
-//        slackHttpClient.send(sb.toString());
+        StringBuilder sb = new StringBuilder();
+        sb.append("실시간 데이터 ");
+        sb.append(market);
+        sb.append(" 비트코인 가격 : " );
+        sb.append(formatPrice);
+        slackHttpClient.send(sb.toString());
+
+        // 메세지 내역 저장
         repository.save(market, String.valueOf(tickerByMarket.getTrade_price()));
     }
 
