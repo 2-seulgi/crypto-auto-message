1312724e73fa845809be2053256fa5747571c0e1
2-seulgi <90025449+2-seulgi@users.noreply.github.com>
2024-08-06 01:24:55 +0900

    💚 로그 추가 및 크론탭 변경

--- src/main/java/com/toy/cryptoautomessage/http/SlackHttpClient.java
+++ src/main/java/com/toy/cryptoautomessage/http/SlackHttpClient.java
@@ -4,18 +4,28 @@ import com.slack.api.Slack;
 import com.slack.api.webhook.Payload;
 import org.springframework.stereotype.Component;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 
 @Component
 public class SlackHttpClient {
+    private static final Logger logger = LoggerFactory.getLogger(SlackHttpClient.class);
+
     public void send(String message){
         try {
+            logger.info("Sending Slack message: " + message);
+
             Slack instance = Slack.getInstance();
             Payload payload = Payload.builder()
                     .text(message)
                     .build();
             instance.send("https://hooks.slack.com/services/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", payload);
+            logger.info("메세지 전송 성공");
+
         }catch (IOException e){
+            logger.error("메세지 전송 실패", e);
             throw new RuntimeException(e);
         }
     }
