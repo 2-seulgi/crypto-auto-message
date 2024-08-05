94b87bcd936a0e7bf6b1e881ed6214b49e87d5f5
2-seulgi <90025449+2-seulgi@users.noreply.github.com>
2024-08-06 01:04:16 +0900

    🐛 깃허브액션 빌드 오류 수정

--- src/main/java/com/toy/cryptoautomessage/CryptoAutoMessageApplication.java
+++ src/main/java/com/toy/cryptoautomessage/CryptoAutoMessageApplication.java
@@ -2,12 +2,15 @@ package com.toy.cryptoautomessage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.lang.module.Configuration;
 
 @SpringBootApplication
 public class CryptoAutoMessageApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CryptoAutoMessageApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(CryptoAutoMessageApplication.class, args);
+        SpringApplication.exit(run);
     }
-
 }
